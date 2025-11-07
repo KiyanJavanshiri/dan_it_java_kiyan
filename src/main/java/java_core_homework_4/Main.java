55e36e7a2a7ae55bed5e0d19c56d9a89fb3d75c5
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2025-11-07 20:07:08 +0200

    feat: optimize main class

--- src/main/java/java_core_homework_4/Main.java
+++ src/main/java/java_core_homework_4/Main.java
@@ -2,23 +2,22 @@ package java_core_homework_4;
 
 public class Main {
     public static void main(String[] args) {
-//        Example usage of methods
+        // Example usage of methods
         String[][] schedule = createSchedule();
-        Pet pet1 = new Pet();
-        Pet pet2 = new Pet(Species.DOG, "Jack");
-        Pet pet3 = new Pet(Species.CAT, "Gwen", 2, 70, new String[] {"eat", "drink", "sleep"});
+
+        Pet pet = new Pet(Species.CAT, "Gwen", 2, 70, new String[] {"eat", "drink", "sleep"});
 
         Human person1 = new Human();
         Human person2 = new Human("Jack", "Hamilton", 24);
-        Human person3 = new Human("Lisa", "Hamilton", 40, pet3, 90, schedule);
-        System.out.println(person1.toString());
-        System.out.println(person2.toString());
-        System.out.println(person3.toString());
+        Human person3 = new Human("Lisa", "Hamilton", 40, pet, 90, schedule);
 
-        Family family1 = new Family(person2, person3);
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+
         System.out.println("---------------------------");
-//        Creating family with child
 
+        // Creating family with child
         Pet jesse = new Pet(Species.CAT, "Jesse", 2, 70, new String[] {"eat", "drink", "sleep"});
 
         Human mother = new Human("Jane", "Karleone", 1975, null, 80, schedule);
@@ -27,11 +26,10 @@ public class Main {
         Family family = new Family(mother, father);
         family.setPet(jesse);
 
-
         Human child1 = new Human("Misha", "Karleone", 2000, jesse, 68, schedule);
         family.addChild(child1);
 
-        System.out.println(child1.toString());
+        System.out.println(child1);
 
         child1.greetPet();
         child1.describePet();
@@ -41,13 +39,10 @@ public class Main {
         jesse.foul();
         jesse.eat();
 
-//        Human child2 = new Human("Olena", "Karleone", 2004, jesse, 58, schedule);
-//        family.addChild(child2);
-
-        System.out.println(family.toString());
+        System.out.println(family);
         System.out.println(family.countFamily());
         System.out.println(family.deleteChild(1));
-        System.out.println(family.toString());
+        System.out.println(family);
         System.out.println(family.countFamily());
     }
 
