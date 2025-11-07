6f38c91ad82c5883256d3347c187295ac013cfa8
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2025-11-07 20:03:31 +0200

    fix: remove unnecessary toString and variables

--- src/main/java/java_core_homework_3/Main.java
+++ src/main/java/java_core_homework_3/Main.java
@@ -2,23 +2,22 @@ package java_core_homework_3;
 
 public class Main {
     public static void main(String[] args) {
-//        Example usage of methods
+        // Example usage of methods
         String[][] schedule = {{"Monday", "yoga"}, {"Friday", "reading"}};
-        Pet pet1 = new Pet();
-        Pet pet2 = new Pet("Dog", "Jack");
-        Pet pet3 = new Pet("Cat", "Gwen", 2, 70, new String[] {"eat", "drink", "sleep"});
+
+        Pet pet = new Pet("Cat", "Gwen", 2, 70, new String[] {"eat", "drink", "sleep"});
 
         Human person1 = new Human();
         Human person2 = new Human("Jack", "Hamilton", 24);
-        Human person3 = new Human("Lisa", "Hamilton", 40, pet3, 90, schedule);
-        System.out.println(person1.toString());
-        System.out.println(person2.toString());
-        System.out.println(person3.toString());
+        Human person3 = new Human("Lisa", "Hamilton", 40, pet, 90, schedule);
 
-        Family family1 = new Family(person2, person3);
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+
         System.out.println("---------------------------");
-//        Creating family with child
 
+        // Creating family with child
         Pet jesse = new Pet("Cat", "Jesse", 2, 70, new String[] {"eat", "drink", "sleep"});
 
         Human mother = new Human("Jane", "Karleone", 1975, null, 80, schedule);
@@ -27,11 +26,10 @@ public class Main {
         Family family = new Family(mother, father);
         family.setPet(jesse);
 
-
         Human child1 = new Human("Misha", "Karleone", 2000, jesse, 68, schedule);
         family.addChild(child1);
 
-        System.out.println(child1.toString());
+        System.out.println(child1);
 
         child1.greetPet();
         child1.describePet();
@@ -44,10 +42,10 @@ public class Main {
         Human child2 = new Human("Olena", "Karleone", 2004, jesse, 58, schedule);
         family.addChild(child2);
 
-        System.out.println(family.toString());
+        System.out.println(family);
         System.out.println(family.countFamily());
         System.out.println(family.deleteChild(1));
-        System.out.println(family.toString());
+        System.out.println(family);
         System.out.println(family.countFamily());
     }
 }
