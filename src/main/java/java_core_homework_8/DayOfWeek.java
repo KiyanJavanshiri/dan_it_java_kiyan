0b55b3cc498bcef68aa10f0d90e7a4947c39bd1b
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2025-11-16 18:21:27 +0200

    feat: add previous project classes and create FamilyDao

---
+++ src/main/java/java_core_homework_8/DayOfWeek.java
@@ -0,0 +1,11 @@
+package java_core_homework_8;
+
+public enum DayOfWeek {
+    SATURDAY,
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY;
+}
---
+++ src/main/java/java_core_homework_8/Woman.java
@@ -0,0 +1,28 @@
+package java_core_homework_8;
+
+import java.util.Map;
+
+public final class Woman extends Human {
+    public Woman() {
+        super();
+    }
+
+    public Woman(String name, String surname) {
+        super(name, surname);
+    }
+
+    public Woman(String name, String surname, Pet pet, int iq, Map<String, String> schedule) {
+        super(name, surname, pet, iq, schedule);
+    }
+
+    @Override
+    public void greetPet() {
+        if(!(getPet() == null)) {
+            System.out.println("Hello my darling cutie " + getPet().getNickname() + "from a woman " + getName());
+        }
+    }
+
+    public void makeUp() {
+        System.out.println("I need to powder my face and dye my eyelashes");
+    }
+}
