8d1b4618669ea85c1dd1636235f4c2718b594251
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2025-11-25 18:59:48 +0200

    fix: fix some minor details

--- src/main/java/java_core_homework_10/ApplicationConsole.java
+++ src/main/java/java_core_homework_10/ApplicationConsole.java
@@ -122,6 +122,7 @@ public class ApplicationConsole {
                         break;
                     }
                 }
+                break;
             case "8":
                 editFamilyMenu();
                 break;
